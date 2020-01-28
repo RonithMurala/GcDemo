5cff92a9a4a5149460145aeec00c7f069cb7e4ed
Ronith Murala <60218744+RonithMurala@users.noreply.github.com>
2020-01-28 14:25:02 +0530

    Add files via upload

---
+++ DisposeSample.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+
+namespace GcDemo
+{
+
+    public class DisposeSample
+    {
+        class MyResources : IDisposable
+        {
+           
+            TextReader tr = null;
+                   
+            public MyResources(string path)
+            {
+           
+                Console.WriteLine("Aquiring Managed Resources");
+                tr = new StreamReader(path);
+
+        
+                Console.WriteLine("Aquiring Unmanaged Resources");
+
+            }
+
+            void ReleaseManagedResources()
+            {
+                Console.WriteLine("Releasing Managed Resources");
+                if (tr != null)
+                {
+                    tr.Dispose();
+                }
+            }
+
+            void ReleaseUnmangedResources()
+            {
+                Console.WriteLine("Releasing Unmanaged Resources");
+            }
+
+            public void ShowData()
+            {
+     
+                if (tr != null)
+                {
+                    Console.WriteLine(tr.ReadToEnd() + " /some unmanaged data ");
+                }
+            }
+
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose called from outside");
+
+                Dispose(true);
+
+     
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                Console.WriteLine("Actual Dispose called with a " + disposing.ToString());    
+                if (disposing == true)
+                {
+
+                    ReleaseManagedResources();
+                }
+                else
+                {
+
+                }   
+
+     
+                ReleaseUnmangedResources();
+
+            }
+
+            ~MyResources()
+            {
+                Console.WriteLine("Finalizer called");
+
+                Dispose(false);
+            }
+        }
+        public static void Main()
+        {
+            MyResources r = null;
+
+            try
+            {
+                r = new MyResources(@"Files\test.txt");
+                r.ShowData();
+            }
+            finally
+            {
+                r.Dispose();
+            }
+        }
+    }
+}
